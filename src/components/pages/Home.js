f318402561c7c00743197a9b2511be9a7e58443c
patrick <patrickcbrito88@gmail.com>
2024-02-22 11:04:15 -0300

    feat: criacao home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,16 @@
+import styles from "./Home.module.css"
+import escola from "../../img/escola.png"
+import LinkButton from "../../layout/LinkButton";
+
 function Home(){
-    return<h1>Home</h1>
+    return(
+        <section className={styles.home_container}>
+            <h1>Bem vindo à <span>Gestão Ensino</span></h1>
+            <p>Comece a gerenciar sua escola agora mesmo!</p>
+            <LinkButton to="/newproject" text="New Project" />
+            <img src={escola} alt="Gestão Ensino"/>
+        </section>
+    )
 }
 
 export default Home
